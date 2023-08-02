62f5f674bb35f46fd106ec38751fe97925d8401f
Marcelino Bautista <mb71@hawaii.edu>
2023-08-02 13:19:13 -1000

    creating the main function

--- index.py
+++ index.py
@@ -2,4 +2,40 @@
     Game to be create based on youtube channel 
 '''
 
-print("hello world ")
+import pygame 
+import random 
+import math 
+import os 
+from os import listdir 
+from os.path import isfile, join 
+
+pygame.init()
+
+pygame.display.set_caption("Platformer")
+
+BG_COLOR = (255, 255, 255)
+WIDTH, HEIGHT = 1000, 800
+FPS = 60 
+
+PLAYER_VEL = 5 
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(window):
+    clock = pygame.time.Clock()
+    run = True 
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break 
+    
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main(window)
+
