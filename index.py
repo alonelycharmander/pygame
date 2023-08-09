7f50e3e605808219374e5ec1ee91109344acc40c
Marcelino Bautista <mb71@hawaii.edu>
2023-08-08 20:07:30 -1000

    implemented gravity

--- index.py
+++ index.py
@@ -20,13 +20,15 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
+    GRAVITY = 1
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
         self.y_vel = 0 
         self.mask = None 
         self.direction = "left"
-        self.animation_count = 0 
+        self.animation_count = 0
+        self.fall_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx 
@@ -45,12 +47,13 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
     
     def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
+
+        self.fall_count += 1 
 
     def draw(self, win):
         pygame.draw.rect(win, self.COLOR, self.rect)
-
-
 
 
 
